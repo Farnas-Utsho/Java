3fe5c7d0ab390899a2c394a3b25b992bd79795a5
Farnas Utsho <mutsho201176@bscse.uiu.ac.bd>
2024-02-02 17:45:46 +0600

    Variables

--- main.java
+++ main.java
@@ -3,8 +3,19 @@
  */
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World2");
+        // System.out.println("Hello World");
+        // System.out.println("Hello World2");
+        //Primitive data type=Not changeable
+        int Hello_World = 10;
+        double a = 20.90 ;
+        boolean b = true;
+        char c = 't' ;
+        String str = "My name is Utsho" ;
+        System.out.println(str);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(Hello_World);
+        System.out.println(5); 
     }
     
 }
